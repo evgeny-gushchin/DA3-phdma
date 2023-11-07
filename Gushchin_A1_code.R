cd798a108a996a49ab48c6df59c6949e08a23fb7
Evgeny Gushchin <gushchin_evgeny@phd.ceu.edu>
2023-11-07 21:10:58 +0300

    Update Gushchin_A1_code.R

--- Gushchin_A1_code.R
+++ Gushchin_A1_code.R
@@ -1,8 +1,24 @@
 ### Assignment 1 Prediction with ML for Economists Fall 2023 ###
+rm(list = ls()) # cleaning the environment
+
+library(ggplot2)
+library(dplyr)
+#library(tidyverse)
 
 # open data
+data_original <- read.csv("morg-2014-emp.csv")
+data_original %>% glimpse()
 
-# filter data
+# filter and process data
+data <- data_original %>% filter(occ2012 == 4110) 
+# we are keeping only the occupation of our interest Waiters and waitresses
+data %>% glimpse()
+data %>% dim() # we end up with 2178 observations
+
+# now let's figure out what data do we have
+length(unique(data$X)) # all unique -> X must be the ID variable
+# hhid - Household id
+length(unique(data$hhid)) # we have 2055 unique HH
 
 # models 1-4
 
