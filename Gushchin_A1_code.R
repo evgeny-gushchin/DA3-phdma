c73378d23745e886763c2bf6955b47e04a9afebf
Evgeny Gushchin <gushchin_evgeny@phd.ceu.edu>
2023-11-07 14:20:24 +0300

    Create Gushchin_A1_code.R

---
+++ Gushchin_A1_code.R
@@ -0,0 +1,16 @@
+### Assignment 1 Prediction with ML for Economists Fall 2023 ###
+
+# open data
+
+# filter data
+
+# models 1-4
+
+# calculate RSME
+
+# clculate cross-validated RMSE
+
+# calculate BIC
+
+# visualize the results
+
