d640d90810d0c0bbe9e18b74dc6df0a9bf31e7df
Evgeny Gushchin <gushchin_evgeny@phd.ceu.edu>
2023-12-12 00:34:38 +0100

    target variable

--- A3/Gushchin_A3_code.R
+++ A3/Gushchin_A3_code.R
@@ -27,6 +27,7 @@ library(gmodels)
 library(lspline)
 library(sandwich)
 library(modelsummary)
+library(tidyr)
 
 library(rattle)
 library(caret)
@@ -59,7 +60,89 @@ data <- data_original %>%
   filter(year <2016 & year >2009) # thus we end up with the sample of 2010-2015
 data %>% glimpse() # 167,606 rows and 42 columns
 
+summary(data$exit_year)
+length(data$exit_year[is.na(data$exit_year)]) # probably it is better to drop companies that have exited
+
+data <- data %>% filter(is.na(exit_year))
+data %>% glimpse() # now we have 150,648 observations
+
+# add all missing year and comp_id combinations -
+# originally missing combinations will have NAs in all other columns
+data <- data %>%
+  complete(year, comp_id)
+data %>% glimpse() # now we have 201,816 observations
+
 # creating our target variable (fast growth)
+summary(data$sales)
+length(data$sales[data$sales < 0]) # some negative numbers, though not too many
+
+summary(data$fixed_assets)
+length(data$fixed_assets[data$fixed_assets < 0]) # few negative numbers
+length(data$fixed_assets[data$fixed_assets == 0]) # a few zeros
+
+summary(data$inc_bef_tax)
+length(data$inc_bef_tax[data$inc_bef_tax < 0]) # a half of observations has negative income
+
+summary(data$share_eq)
+length(data$share_eq[data$share_eq < 0]) # almost a half of observations has negative shareholder equity
+
+# Option 1: The fixed asset turnover ratio (FAT)
+#FAT is useful in determining whether a company is efficiently using its fixed assets to drive net sales. 
+#FAT is calculated by dividing net sales by the average balance of fixed assets of a period. 
+#Though the ratio is helpful as a comparative tool over time or against other companies, it fails to identify unprofitable companies.
+#https://www.investopedia.com/terms/f/fixed-asset-turnover.asp
+
+data <- data %>%
+  mutate(sales = ifelse(sales < 0, 1, sales),
+         ln_sales = ifelse(sales > 0, log(sales), 0),
+         sales_mil=sales/1000000,
+         sales_mil_log = ifelse(sales > 0, log(sales_mil), 0))
+
+data <- data %>%
+  mutate(fixed_assets = ifelse(fixed_assets < 0, 1, fixed_assets),
+         ln_fixed_assets = ifelse(fixed_assets > 0, log(fixed_assets), 0),
+         fixed_assets_mil=fixed_assets/1000000,
+         fixed_assets_mil_log = ifelse(fixed_assets > 0, log(fixed_assets_mil), 0))
+
+data <- data %>% mutate(fat = ifelse(fixed_assets != 0, sales/fixed_assets, 0))
+summary(data$fat)
+
+data <- data %>%
+  mutate(fat = ifelse(fat < 0, 1, fat),
+         ln_fat = ifelse(fat > 0, log(fat), 0))
+
+data <- data %>%
+  group_by(comp_id) %>%
+  mutate(d1_ln_fat = ln_fat - lag(ln_fat, 1) ) %>%
+  ungroup()
+
+summary(data$d1_ln_fat)
+data %>% filter(!is.na(d1_ln_fat) & d1_ln_fat>5) %>% dim() # lets say that 5% growth rate of FAT is out threshold for fast growing firm
+
+data  <- data %>%
+  mutate(fast_growth = ifelse(d1_ln_fat > 5 & !is.na(d1_ln_fat),1,0))
+table(data$fast_growth) # we have just 1166 of fast growing firms
+table(data$fast_growth, data$year)
+
+data <- data %>% 
+  group_by(comp_id) %>%
+  mutate(future_fast_growth = lead(fast_growth,1)) %>% 
+  ungroup()
+table(data$future_fast_growth, data$year)
+
+# Option 3: Growth of number of employees (labor_avg)
+# we have too many missing values (~50%)
+
+# Option 4: Growth rate of Shareholder equity (share_eq)
+# might have nothing to do with the company's efficiency
+
+# look at cross section
+data <- data %>%
+  filter(year == 2012) %>%
+  # look at firms below 10m euro revenues and above 1000 euros
+  filter(!(sales_mil > 10)) %>%
+  filter(!(sales_mil < 0.001))
+table(data$future_fast_growth) #very unbalanced
 
 # Model 1: Logit
 
